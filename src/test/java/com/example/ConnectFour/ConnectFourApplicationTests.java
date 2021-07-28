f91a50026cd4434765850f3761c78f15c38e5b43
Xingyu Yue <yuexingyu2001@gmail.com>
2021-07-28 15:46:34 -0400

    Initial commit

---
+++ src/test/java/com/example/ConnectFour/ConnectFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConnectFour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
