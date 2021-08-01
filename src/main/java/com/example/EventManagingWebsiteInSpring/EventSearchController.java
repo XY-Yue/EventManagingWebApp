2e80737b3d64cb0cf161db0c72f5820eef501454
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-01 16:54:08 -0400

    Tried to use thymeleaf to send data in a form through requests.

---
+++ src/main/java/com/example/EventManagingWebsiteInSpring/EventSearchController.java
@@ -0,0 +1,16 @@
+package com.example.EventManagingWebsiteInSpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventSearchController {
+
+    @PostMapping("/searchEvent")
+    public String searchEvent(Model model){
+        return "SearchEvent";
+    }
+}
---
+++ src/main/java/com/example/EventManagingWebsiteInSpring/controller/SearchFieldContainer.java
@@ -0,0 +1,5 @@
+package com.example.EventManagingWebsiteInSpring.controller;
+
+public class SearchFieldContainer {
+
+}
