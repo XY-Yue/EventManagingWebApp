b75520ff620466391086d671bbe12dab82b048da
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-03 16:37:43 -0400

    Added some html, learned to use a submit type input to send requests.

--- src/main/java/com/example/EventManagingWebsiteInSpring/RoomController.java
+++ src/main/java/com/example/EventManagingWebsiteInSpring/RoomController.java
@@ -2,15 +2,18 @@ package com.example.EventManagingWebsiteInSpring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RoomSearchController {
+public class RoomController {
 
-    @PostMapping("/searchEvent")
+    @PostMapping("/searchRoom")
     public String searchEvent(Model model){
-        return "SearchEvent";
+        return "SearchRoom";
+    }
+
+    @PostMapping("/addRoom")
+    public String addEvent(Model model){
+        return "AddRoom";
     }
 }
