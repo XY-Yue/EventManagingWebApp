2e83047f242dcadec41f5c32f54b07aa56b5809b
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-06 17:38:14 -0400

    Learned to include CSS and icon in html in a spring boot project.

--- src/main/java/com/example/EventManagingWebsiteInSpring/controller/RoomController.java
+++ src/main/java/com/example/EventManagingWebsiteInSpring/controller/RoomController.java
@@ -2,12 +2,13 @@ package com.example.EventManagingWebsiteInSpring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RoomController {
 
-    @PostMapping("/searchRoom")
+    @GetMapping("/searchRoom")
     public String searchEvent(Model model){
         return "SearchRoom";
     }
