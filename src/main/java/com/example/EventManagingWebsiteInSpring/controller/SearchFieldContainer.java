dae43291d43e91b874db37c1c005468f7a8b8e8e
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-12 20:25:32 -0400

    Learning to pass form data through an object.

--- src/main/java/com/example/EventManagingWebsiteInSpring/controller/SearchFieldContainer.java
+++ src/main/java/com/example/EventManagingWebsiteInSpring/controller/SearchFieldContainer.java
@@ -1,5 +1,8 @@
 package com.example.EventManagingWebsiteInSpring.controller;
 
 public class SearchFieldContainer {
-
+    String nameKey;
+    String availableStart;
+    String availableEnd;
+    int capacity;
 }
