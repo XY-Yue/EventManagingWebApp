d6e4d05acfea898dc72bd05ce90a283eab5f97f6
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-17 17:19:24 -0400

    Studied how to send post requests with ajax.

---
+++ src/main/resources/static/js/AddRoom.js
@@ -0,0 +1,9 @@
+let available = [];
+
+for (let i = 0; i < 24; i++){
+    document.getElementById("roomAvailable" + i).addEventListener("click", onChangeAvailableTime);
+}
+
+function onChangeAvailableTime(){
+
+}
