83a5ede5672be69ff1dd927c0dcdd1a03ecb5a59
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-18 23:39:58 -0400

    Implemented the UI for hour selections when adding rooms.

--- src/main/resources/static/js/AddRoom.js
+++ src/main/resources/static/js/AddRoom.js
@@ -1,9 +1,35 @@
 let available = [];
+const SELECTED = "forestgreen";
+const NOT_SELECTED = "white";
 
 for (let i = 0; i < 24; i++){
     document.getElementById("roomAvailable" + i).addEventListener("click", onChangeAvailableTime);
 }
 
+// Respond user's click event on the buttons in the select hour field, changes its color
 function onChangeAvailableTime(){
+    if (this.style.backgroundColor === SELECTED){
+        this.style.backgroundColor = NOT_SELECTED;
+        removeHourFromAvailable(parseInt(this.innerHTML.toString()));
+    }else {
+        this.style.backgroundColor = SELECTED;
+        addHourToAvailable(parseInt(this.innerHTML.toString()));
+    }
+}
 
+function removeHourFromAvailable(hour){
+    for (let i = 0; i < available.length; i++){
+        if (available[i] === hour) {
+            available.splice(i, 1);
+        }
+    }
+}
+
+function addHourToAvailable(hour){
+    for (let i = 0; i < available.length; i++){
+        if (available[i] > hour) {
+            available.splice(i, 0, hour);
+            break;
+        }
+    }
 }
