ee1241ec717ecb73c00f7dd9fa1bfb3c993ec06f
Xingyu Yue <yuexingyu2001@gmail.com>
2021-08-20 17:32:03 -0400

    Working on code related to using ajax to verify a room name.

--- src/main/java/com/example/EventManagingWebsiteInSpring/controller/RoomController.java
+++ src/main/java/com/example/EventManagingWebsiteInSpring/controller/RoomController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RoomController {
@@ -21,5 +22,18 @@ public class RoomController {
     @PostMapping("/addingRoom")
     public String addingRoom(Model model, RoomInitDTO roomInit){
         return "SearchRoom";
+    }
+
+    @PostMapping("/validateName")
+    public String validateRoomName(Model model, @RequestParam("roomName") String roomName){
+        return "";
+    }
+
+    private boolean validateRoomName(String roomName){
+        return false;
+    }
+
+    private void addRoom(RoomInitDTO roomInitDTO){
+
     }
 }
--- src/main/resources/static/js/AddRoom.js
+++ src/main/resources/static/js/AddRoom.js
@@ -34,3 +34,7 @@ function addHourToAvailable(hour){
         }
     }
 }
+
+function validateInput(){
+
+}
